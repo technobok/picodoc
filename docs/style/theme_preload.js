dd575a980a3eb0d552934f4f9e7ef424526e74c1
technobok <p@technobok.org>
2026-02-23 00:17:48 +1000

    more documenation work

---
+++ docs/style/theme_preload.js
@@ -0,0 +1,7 @@
+<script>
+  <!-- prevent FOUC -->
+  if (localStorage.getItem('theme') === 'dark') {
+    //root.classList.add('dark-mode');
+    document.body.classList.add('dark-mode');
+  };
+</script>
